5d9c3a0bad8009d60937fce17e0b3094df32b5fb
I-Milanov <i.t.milanov1996@gmail.com>
2025-05-28 07:30:19 +0300

    Add resx

--- EcommercePlaygroundTests/Tests/RegistrationFormTests.cs
+++ EcommercePlaygroundTests/Tests/RegistrationFormTests.cs
@@ -1,15 +1,19 @@
-using EcommercePlaygroundTests.Models.FormModels;
+﻿using EcommercePlaygroundTests.Models.FormModels;
 using EcommercePlaygroundTests.Services.ConfigurationService;
 
 namespace EcommercePlaygroundTests.Tests
 {
     public class RegistrationFormTests : BaseTests
     {
-        [Test]
+           [Test]
         public void AssertRegistrationForm_When_OpenRegistrationPage()
         {
-            var setting = ConfigurationService.GetConfiguration();
+            var setting = ConfigurationService.GetConfiguration().Language;
 
+            string homePageText = Strings.Loans;
+            Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("bg");
+            homePageText = Strings.HomePage;
+            var test = Strings.Loans;
             RegistrationPage.Open();
 
             RegistrationPage.Form.AssertFormStructure<RegisterFormModel>();
